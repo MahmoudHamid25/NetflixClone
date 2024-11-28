d247e0b2bc9358394e2ea19d1267dca0cf0028c0
MahmoudHamid25 <mahmoud.hamid@student.nhlstenden.com>
2024-11-28 22:40:41 +0100

    setup-TypeORM

--- netflix-clone/src/app.module.ts
+++ netflix-clone/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // This loads environment variables
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root', // Use  actual password here
+      database: 'netflix-clone', // Your database name
+      entities: [], // You will add your entities here
+      synchronize: true, // Automatically synchronize schema (caution in production)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
