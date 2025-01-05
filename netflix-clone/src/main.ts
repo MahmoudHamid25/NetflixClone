1832c7e5e2e7788af2815ca15e2c01b13e9c78df
unknown <nhlsviatoslav@gmail.com>
2025-01-05 11:57:02 +0100

    docs(main): add comment to main.ts
    desc

--- netflix-clone/src/main.ts
+++ netflix-clone/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  //  comment
 
   const PORT = configService.get<number>('PORT');
   await app.listen(PORT || 4000);
