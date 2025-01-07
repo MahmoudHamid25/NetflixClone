28b53128343611802b6634e106bf5397ff26c216
unknown <nhlsviatoslav@gmail.com>
2025-01-07 09:22:20 +0100

    feat(auth): added cookies parser

--- netflix-clone/src/main.ts
+++ netflix-clone/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log', 'debug'] });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(cookieParser());
+  app.enableCors();
+
 
   const configService = app.get(ConfigService);
-  //  comment
-
   const PORT = configService.get<number>('PORT');
   await app.listen(PORT || 4000);
 }
